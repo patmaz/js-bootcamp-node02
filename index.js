b07b2004b3623ea7b5985f3e8b144eb1203d62fd
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-03-04 07:27:15 +0100

    err hanlder

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var fs = require('fs');
 var http = require('http');
 var server = http.createServer();
 
+process.on('uncaughtException', function(err) {
+    console.error('Uncaught error', err);
+});
+
 server.on('request', function(req, res){
     if(req.method === 'GET' && req.url === '/') {
         fs.readFile('./assets/index.html', function(err, data){
-            if (err) throw err;
             res.writeHead(200, {
                 "Content-Type": "text/html; charset=utf-8"
             });
@@ -14,7 +17,6 @@ server.on('request', function(req, res){
         });
     } else {
         fs.readFile('./assets/404.gif', function(err, data){
-            if (err) throw err;
             res.writeHead(404, {
                 "Content-Type": "image/gif"
             });
